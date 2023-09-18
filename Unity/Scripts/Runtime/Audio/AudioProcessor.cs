d4aea587eed82b41ebfc94fc56a574565d5e7807
Ryan McClelland <rymcclel@gmail.com>
2023-09-18 00:11:29 -0700

    attempt to improve crackling in audio
    the issue here was that the destination buffer was using the same length as the
    source buffer and the destination buffer needs to be longer or shorter based on
    ratio between the input and output sample rates. This is still not a perfect
    solution, and needs to be revisited for further improvement.

--- Unity/Scripts/Runtime/Audio/AudioProcessor.cs
+++ Unity/Scripts/Runtime/Audio/AudioProcessor.cs
@@ -20,20 +20,80 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE. */
 
+using NAudio.Utils;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Unity.Collections;
 using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace SK.Libretro.Unity
 {
+    public class CircularBuffer<T>
+    {
+        private T[] buffer;
+        private int head;
+        private int tail;
+
+        public CircularBuffer(int capacity)
+        {
+            buffer = new T[capacity];
+            head = 0;
+            tail = 0;
+        }
+
+        public int Count
+        {
+            get { return tail >= head ? tail - head : buffer.Length - (head - tail); }
+        }
+
+        public int Capacity
+        {
+            get { return buffer.Length; }
+        }
+
+        public void Enqueue(T item)
+        {
+            buffer[tail] = item;
+            tail = (tail + 1) % buffer.Length;
+            if (tail == head)
+            {
+                head = (head + 1) % buffer.Length;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("Buffer is empty");
+            T item = buffer[head];
+            head = (head + 1) % buffer.Length;
+            return item;
+        }
+
+        public void Clear()
+        {
+            head = 0;
+            tail = 0;
+        }
+
+        public T this[int index]
+        {
+            get { return buffer[(head + index) % buffer.Length]; }
+            set { buffer[(head + index) % buffer.Length] = value; }
+        }
+    }
+
     [RequireComponent(typeof(AudioSource)), DisallowMultipleComponent]
     internal sealed class AudioProcessor: MonoBehaviour, IAudioProcessor
     {
-        private const int AUDIO_BUFFER_SIZE = 65536;
+        private const int AUDIO_BUFFER_SIZE = 262144;
+        private const int CIRCULAR_BUFFER_SIZE = AUDIO_BUFFER_SIZE * 2;
 
-        private readonly List<float> _audioBufferList = new(AUDIO_BUFFER_SIZE);
+        private readonly List<float> _audioBufferList = new List<float>(AUDIO_BUFFER_SIZE);
+        private CircularBuffer<float> _circularBuffer = new CircularBuffer<float>(CIRCULAR_BUFFER_SIZE);
 
         private AudioSource _audioSource;
         private int _inputSampleRate;
@@ -46,17 +106,20 @@ namespace SK.Libretro.Unity
 
         private void OnAudioFilterRead(float[] data, int channels)
         {
-            if (_audioBufferList.Count < data.Length)
+            if (_circularBuffer.Count < data.Length)
                 return;
 
+            // Read samples from circular buffer
             for (int i = 0; i < data.Length; ++i)
-                data[i] = _audioBufferList[i];
-
-            _audioBufferList.RemoveRange(0, data.Length);
+            {
+                data[i] = _circularBuffer.Dequeue();
+            }
         }
+
 
         public void Init(int sampleRate) => MainThreadDispatcher.Enqueue(() =>
         {
+            Debug.Log($"Set sample to {sampleRate}");
             _inputSampleRate  = sampleRate;
             _outputSampleRate = AudioSettings.outputSampleRate;
 
@@ -67,6 +130,7 @@ namespace SK.Libretro.Unity
             if (_samples.IsCreated)
                 _samples.Dispose();
 
+            _circularBuffer.Clear();
             _audioSource.Play();
         });
 
@@ -90,23 +154,28 @@ namespace SK.Libretro.Unity
             _audioBufferList.AddRange(_samples);
         });
 
-        public unsafe void ProcessSampleBatch(IntPtr data, nuint frames) => MainThreadDispatcher.Enqueue(() =>
+        public unsafe void ProcessSampleBatch(IntPtr data, nuint frames)
         {
+            if ((_inputSampleRate == 0) || (_outputSampleRate == 0))
+                return;
+
             if (!_jobHandle.IsCompleted)
                 _jobHandle.Complete();
+            //Debug.Log($"Sample Rate: in:{_inputSampleRate}Hz out:{_outputSampleRate}Hz");
+            double ratio = (double)_outputSampleRate / (double)_inputSampleRate;
+            int numSourceFrames = (int)frames * 2;
+            int numDestFrames = (int)((double)(numSourceFrames) * ratio);
+            //Debug.Log($"create buffer of {numDestFrames} from {frames} {ratio}");
+            CreateBuffer(numDestFrames);
 
-            int numFrames = (int)frames * 2;
-            CreateBuffer(numFrames);
-            _jobHandle = new SampleBatchJob
+            Resampler.LinearResample((short*)data, numSourceFrames, _samples, numDestFrames, _inputSampleRate, _outputSampleRate);
+
+            // Add samples to circular buffer
+            for (int i = 0; i < numDestFrames; i++)
             {
-                SourceSamples      = (short*)data,
-                DestinationSamples = _samples,
-                SourceSampleRate   = _inputSampleRate,
-                TargetSampleRate   = _outputSampleRate
-            }.Schedule(numFrames, 64);
-            _jobHandle.Complete();
-            _audioBufferList.AddRange(_samples);
-        });
+                _circularBuffer.Enqueue(_samples[i]);
+            }
+        }
 
         private void CreateBuffer(int numFrames)
         {
