8cf192a89057a988b05a46c3d16efad372979e04
Erik Koopmans <erik@erik-koopmans.com>
2026-03-18 23:56:06 -0400

    Fix missing wrapper in bridge commands

--- Unity/Scripts/Runtime/Bridge.cs
+++ Unity/Scripts/Runtime/Bridge.cs
@@ -403,19 +403,19 @@ namespace SK.Libretro.Unity
             => _bridgeCommands.Enqueue(new SaveStateWithScreenshotBridgeCommand(_wrapper, TakeScreenshot));
 
         public void SaveStateWithoutScreenshot()
-            => _bridgeCommands.Enqueue(new SaveStateWithoutScreenshotBridgeCommand());
+            => _bridgeCommands.Enqueue(new SaveStateWithoutScreenshotBridgeCommand(_wrapper));
 
         public void LoadState()
-            => _bridgeCommands.Enqueue(new LoadStateBridgeCommand());
+            => _bridgeCommands.Enqueue(new LoadStateBridgeCommand(_wrapper));
 
         public void SetDiskIndex(int index)
             => _bridgeCommands.Enqueue(new SetDiskIndexBridgeCommand(_wrapper, _gamesDirectory, _gameNames, index));
 
         public void SaveSRAM()
-            => _bridgeCommands.Enqueue(new SaveSRAMBridgeCommand());
+            => _bridgeCommands.Enqueue(new SaveSRAMBridgeCommand(_wrapper));
 
         public void LoadSRAM()
-            => _bridgeCommands.Enqueue(new LoadSRAMBridgeCommand());
+            => _bridgeCommands.Enqueue(new LoadSRAMBridgeCommand(_wrapper));
 
         public void SaveOptions(bool global)
             => _bridgeCommands.Enqueue(new SaveOptionsBridgeCommand(_wrapper, global));
